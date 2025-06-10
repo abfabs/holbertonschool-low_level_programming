6a8029ebd4fde64d45fe9b918e54262dc74e2a8a
abfabs <albaeftimi@gmail.com>
2025-06-10 17:45:36 +0000

    push all

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the whole alphabet
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
