46c22bc55c16f24923c8a7f5880a344cc5063c65
abfabs <albaeftimi@gmail.com>
2025-06-10 11:22:20 +0000

    push all

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -11,18 +15,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{
-	  printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-	  printf("%d is zero\n", n);
-	}
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
 	else
 	{
 	  printf("%d is negative\n", n);
 	}
-	
+
 		return (0);
 }
