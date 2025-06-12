b8f069e6632e806c4a4a18e1cae7032015e52d51
abfabs <albaeftimi@gmail.com>
2025-06-12 23:05:05 +0000

    push all

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include"main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - Checks for lowercase
+ * @c: character
+ * Return: 1 if c is lowercase, 0 if else
  */
 
 int _islower(int c)
