12bfca84a3b11be169a08bd84f585d1395d01459
abfabs <albaeftimi@gmail.com>
2025-06-14 18:49:09 +0000

    push all

--- 0_exercises_functions_nested_loops/3-calculateGrade.c
+++ 0_exercises_functions_nested_loops/3-calculateGrade.c
@@ -1,4 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+char calculateGrade(int score)
+{
+    if (score >= 90)
+        return 'A';
+    else if (score >= 80)
+        return 'B';
+    else if (score >= 70)
+        return 'C';
+    else if (score >= 60)
+        return 'D';
+    else (score >= 50)
+        return 'F';
+}
+
 
--- 0_exercises_functions_nested_loops/3-main.c
+++ 0_exercises_functions_nested_loops/3-main.c
@@ -1,2 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+
+int main()
+{
+    calculateGrade(98);
+
+    printf("Your grade is %c\n", grade)
+    return 0;
+}
