1937bf32578769e0ac6fd4e1fc4075b3d0bf26ca
abfabs <albaeftimi@gmail.com>
2025-06-16 14:51:21 +0000

    push all

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
+/**
+ * print_diagonal - Draws a diagonal line on the terminal
+ * @n: Number of times the character '\' should be printed
+ *
+ * Return: void
+ */
 void print_diagonal(int n)
 {
-    
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		/* Print spaces before the '\' */
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
