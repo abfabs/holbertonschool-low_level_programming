424e68eab45c06d0aea85f824ba1d16697a1f5dc
abfabs <albaeftimi@gmail.com>
2025-06-16 14:56:06 +0000

    push all

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
+/**
+ * print_triangle - Prints a triangle of '#' characters
+ * @size: The size of the triangle (height and base width)
+ *
+ * Return: void
+ */
 void print_triangle(int size)
 {
-    
+	int row, col, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		/* Print spaces before the '#' */
+		for (spaces = size - row; spaces > 0; spaces--)
+			_putchar(' ');
+
+		/* Print '#' characters */
+		for (col = 0; col < row; col++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
 }
--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
+/**
+ * print_square - Prints a square of '#' characters
+ * @size: The size of the square (number of rows and columns)
+ *
+ * Return: void
+ */
 void print_square(int size)
 {
-    
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
