3283230e25c3e9c15b9e687a4bccd11eef363f50
abfabs <albaeftimi@gmail.com>
2025-06-16 10:55:26 +0000

    push all

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-int _isupper(int c); //0
-int _isdigit(int c); //1
-int mul(int a, int b); //2
-void print_numbers(void); //3
-void print_most_numbers(void); //4
-void more_numbers(void); //5
-void print_line(int n); //6
-void print_diagonal(int n); //7
-void print_square(int size);m //8
-void print_triangle(int size); //10
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
