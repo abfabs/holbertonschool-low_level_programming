df6bd283e1d3e04a29819a56b0c4dff04902044e
abfabs <albaeftimi@gmail.com>
2025-06-16 14:47:03 +0000

    push all

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
-void print_line(int n);
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: Number of times to print the character '_'
+ *
+ * Return: void
+ */
+void print_line(int n)
 {
-    
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+	_putchar('\n');
 }
