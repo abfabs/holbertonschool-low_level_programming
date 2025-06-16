7faab783fe7d35ef3d80fdbc7fce2253c4375f88
abfabs <albaeftimi@gmail.com>
2025-06-16 14:43:47 +0000

    push all

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,30 @@
 #include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9, except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char c;
+	char output[9]; /* 8 digits + newline */
+	int i = 0;
+	int j;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+		{
+			output[i] = c;
+			i++;
+		}
+	}
+	output[i] = '\n';
+	i++;
+
+	for (j = 0; j < i; j++)
+	{
+		_putchar(output[j]);
+	}
+}
