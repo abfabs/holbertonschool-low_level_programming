fc81b3261c8ce7185077a9779ba109761806f6d0
abfabs <albaeftimi@gmail.com>
2025-06-17 09:57:15 +0000

    push all

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int i, length;
+
+	length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	return (length);
+}
