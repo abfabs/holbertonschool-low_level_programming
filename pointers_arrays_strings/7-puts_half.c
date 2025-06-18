341557c50c6d6fcc561a25fbe153199e451ec223
abfabs <albaeftimi@gmail.com>
2025-06-18 09:51:45 +0000

    push all

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
+/**
+ * puts_half - Prints the second half of a string
+ * @str: Pointer to the string
+ */
 void puts_half(char *str)
 {
 	int length_of_the_string, n;
-	 for (length_of_the_string = 0; s[length_of_the_string] != 0; length_of_the_string++)
-    {
 
-    }
-	if(length_of_the_string % 2 == 0)
+	for (length_of_the_string = 0; str[length_of_the_string] != '\0'; length_of_the_string++)
 	{
-		n = (length_of_the_string + 1) / 2;
 	}
+
+	if (length_of_the_string % 2 == 0)
+	{
+		n = length_of_the_string / 2;
+	}
+	else
+	{
+		n = (length_of_the_string - 1) / 2 + 1;
+	}
+
 	while (n < length_of_the_string)
 	{
 		_putchar(str[n]);
 		n++;
 	}
+	_putchar('\n');
 }
