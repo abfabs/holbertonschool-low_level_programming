9c7ad5206a678c015addc110c70c8cd4abd13707
abfabs <albaeftimi@gmail.com>
2025-06-19 09:43:31 +0000

    push all

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to use from src
+ *
+ * Return: A pointer to the resulting string dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
