a60056d66ee8aaab1588569573e875b0d74dc9d6
abfabs <albaeftimi@gmail.com>
2025-06-19 10:41:31 +0000

    push all

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,31 @@
-#include "main.h"
-
+#include"main.h"
 /**
- * _atoi - Converts a string to an integer
- * @s: The string to be converted
- *
- * Return: The integer value of the string, or 0 if no numbers are found
+ * _atoi -entry point,convert a string to integer
+ * @s: -holds value for output
+ * Return: (sign * number)
  */
+
+
 int _atoi(char *s)
 {
-	int i = 0;
+	int i;
 	int sign = 1;
-	int result = 0;
-	int started = 0;
+	unsigned int number = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == '-' && !started)
-			sign *= -1;
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			started = 1;
-			if ((result > __INT_MAX__ / 10) ||
-			    (result == __INT_MAX__ / 10 && (s[i] - '0') > __INT_MAX__ % 10))
-			{
-				/* Integer overflow, return boundary */
-				if (sign == 1)
-					return (__INT_MAX__);
-				else
-					return (-__INT_MAX__ - 1);
-			}
-			result = result * 10 + (s[i] - '0');
+			number = s[i] - '0' + number * 10;
 		}
-		else if (started)
+		else if (number > 0)
 			break;
-		i++;
 	}
+	return (sign * number);
 
-	return (result * sign);
 }
+
