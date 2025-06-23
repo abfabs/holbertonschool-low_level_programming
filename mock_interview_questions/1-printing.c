888c42ce12cd267ee256afb4f51440a6343c8ad4
abfabs <albaeftimi@gmail.com>
2025-06-23 16:55:17 +0000

    push all

---
+++ mock_interview_questions/1-printing.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 25;
+    float height = 1.74;
+    char grade = 'A';
+    char name[] = "BB";
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f m\n", height);
+    printf("Grade: %c\n", grade);
+    printf("Name: %s\n", name);
+
+    return 0;
+}
