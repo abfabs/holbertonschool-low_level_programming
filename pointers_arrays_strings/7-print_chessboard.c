f69c2d1303af5b7b2b066283cdac9b5d94d4fc41
abfabs <albaeftimi@gmail.com>
2025-06-24 11:16:36 +0000

    push all

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,5 +6,10 @@ void print_diagsums(int *a, int size)
 int sum1, sum2 = 0;
 int i;
 	for (i = 0; i < size * size; i += size + 1)
-	
+		sum1 = sum1 + a[i];
+
+	for (i = size - 1; i < size * size; i += size -1)
+		sum2 = sum2 + a[i];
+
+	printf("%d, %d\n", sum1, sum2);
 }
