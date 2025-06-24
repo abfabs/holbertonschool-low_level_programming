601a32c649b49b801f80f351f174843e54750344
abfabs <albaeftimi@gmail.com>
2025-06-24 11:33:30 +0000

    push all

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,3 +24,4 @@ char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
