78a669d856744a6dc1be21dbeeebd60eab915039
abfabs <albaeftimi@gmail.com>
2025-06-24 11:28:38 +0000

    push all

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,3 +22,4 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
