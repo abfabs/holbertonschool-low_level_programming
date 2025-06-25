a306c5bca670da11d7e42eb0b530a865088edde4
abfabs <albaeftimi@gmail.com>
2025-06-25 12:07:50 +0000

    push all

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - Prints a string in reverse using recursion
+ * @s: Pointer to the string to print
+ */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
