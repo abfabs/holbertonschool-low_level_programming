f2b59880287b4e85c8737cdadb3f048e1b002f46
abfabs <albaeftimi@gmail.com>
2025-06-25 12:09:37 +0000

    push all

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion
+ * @s: Pointer to the string to print
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
