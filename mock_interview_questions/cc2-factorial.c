845842bfdd0aa3beb5499011bb30b3f0f90561aa
abfabs <albaeftimi@gmail.com>
2025-06-28 14:50:55 +0000

    push all

--- mock_interview_questions/cc2-factorial.c
+++ mock_interview_questions/cc2-factorial.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int factorial(int n) {
+int factorial(int n) 
+{
     if (n <= 1)
         return 1;
     else
         return n * factorial(n - 1);
 }
 
-int main() {
+int main() 
+{
     int num = 9;
     printf("Factorial of %d is %d\n", num, factorial(num));
     return 0;
