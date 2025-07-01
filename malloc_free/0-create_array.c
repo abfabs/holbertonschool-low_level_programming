37fa2a6afe476d788193398466032e47ed373d65
abfabs <albaeftimi@gmail.com>
2025-07-01 10:58:05 +0000

    push all

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * create_array - Creates an array of chars and initializes it with a specific char.
+ * create_array - Creates an array of chars and initializes it
+ * with a specific char.
  * @size: Size of the array to be created.
  * @c: Character to initialize the array with.
  *
