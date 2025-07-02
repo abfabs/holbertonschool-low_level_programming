fc9d30b57c594b949f656da2e83265d7db7e5dfb
abfabs <albaeftimi@gmail.com>
2025-07-02 08:44:54 +0000

    push all

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,18 +2,18 @@
 
 /**
  * malloc_checked - Allocates memory using malloc
- * @b: Number of bytes to allocate
+ * @b: The number of bytes to allocate
  *
- * Return: Pointer to allocated memory
- *         Exits with status 98 if malloc fails
+ * Return: A pointer to the allocated memory
+ *         If malloc fails, the process exits with status 98
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
-    if (ptr == NULL)
-        exit(98);
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
 
-    return (ptr);
+	return (ptr);
 }
