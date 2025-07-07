b9079fb1f7ec7f5ca209a99e8238ddb7b292ac1f
abfabs <albaeftimi@gmail.com>
2025-07-07 08:56:32 +0000

    push all

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure that stores information about a dog
+ * @name: Name of the dog (as a string)
+ * @age: Age of the dog (as a float)
+ * @owner: Name of the dog's owner (as a string)
+ *
+ * Description: Defines a struct to hold a dog's name, age, and owner.
+ */
 struct dog
 {
 	char *name;
@@ -8,6 +16,11 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
