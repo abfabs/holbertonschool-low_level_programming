625567237295f9cc5ef94414e1317b630f76eef8
abfabs <albaeftimi@gmail.com>
2025-07-09 19:02:32 +0000

    push all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,6 +1,5 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
-#include <stdio.h>
 
 /**
  * sum_them_all - Returns the sum of all its parameters
@@ -11,16 +10,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    int total = 0;
+	va_list args;
+	unsigned int i;
+	int total = 0;
 
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-    {
-        total += va_arg(args, int);
-    }
-    va_end(args);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		total += va_arg(args, int);
+	}
+	va_end(args);
 
-    return total;
+	return (total);
 }
