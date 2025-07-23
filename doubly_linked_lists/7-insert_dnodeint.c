0509083b4e6096aa38daeff84e6c47aec65f1f2a
abfabs <albaeftimi@gmail.com>
2025-07-23 11:14:44 +0000

    push insert node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,8 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (idx == 0)
-		return (add_dnodeint(h, n)); 
-
+    	return (add_dnodeint(h, n)); 
 	while (temp != NULL && i < idx - 1)
 	{
 		temp = temp->next;
