aa699f5699415f23ac1c073942d17a84a56ee582
abfabs <albaeftimi@gmail.com>
2025-07-24 10:50:16 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,64 +7,65 @@
 
 void close_file(int fd)
 {
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+    if (close(fd) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+        exit(100);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t n_read, n_written;
-	char buffer[BUFFER_SIZE];
+    int fd_from, fd_to;
+    ssize_t n_read, n_written;
+    char buffer[BUFFER_SIZE];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close_file(fd_from);
-		exit(99);
-	}
+    fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        close_file(fd_from);
+        exit(99);
+    }
 
-	while (1)
-	{
-		n_read = read(fd_from, buffer, BUFFER_SIZE);
-		if (n_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close_file(fd_from);
-			close_file(fd_to);
-			exit(98);
-		}
-		if (n_read == 0)
-			break;
+    while (1)
+    {
+        n_read = read(fd_from, buffer, BUFFER_SIZE);
+        if (n_read == -1)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+            close_file(fd_from);
+            close_file(fd_to);
+            exit(98);
+        }
+        if (n_read == 0)
+            break;
 
-		n_written = write(fd_to, buffer, n_read);
-		if (n_written == -1 || n_written != n_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_file(fd_from);
-			close_file(fd_to);
-			exit(99);
-		}
-	}
+        n_written = write(fd_to, buffer, n_read);
+        if (n_written == -1 || n_written != n_read)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            close_file(fd_from);
+            close_file(fd_to);
+            exit(99);
+        }
+    }
 
-	close_file(fd_from);
-	close_file(fd_to);
-	return (0);
+    close_file(fd_from);
+    close_file(fd_to);
+
+    return (0);
 }
